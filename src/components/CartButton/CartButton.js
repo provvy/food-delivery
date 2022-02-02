cfcab5b59f0e233d5bac38be0e042e83c42aac3e
Paolo Provveduto <paoloprovveduto@gmail.com>
2022-02-02 12:42:40 +0100

    added bounce animation to cart button

--- src/components/CartButton/CartButton.js
+++ src/components/CartButton/CartButton.js
@@ -1,4 +1,4 @@
-import { useState, useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 import styles from "./CartButton.module.css";
 import CartIcon from "../../assets/CartIcon";
 import CartModal from "../CartModal/CartModal";
@@ -6,12 +6,33 @@ import { CartContext } from "../context/CartContextProvider";
 
 const CartButton = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isAnimated, setIsAnimated] = useState(false);
   const { state } = useContext(CartContext);
   const cartAmount = state.reduce((a, c) => a + c.amount, 0);
+
+  useEffect(() => {
+    cartAmount !== 0 && setIsAnimated(true);
+  }, [cartAmount]);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setIsAnimated(false);
+    }, 250);
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [isAnimated]);
+
   return (
     <>
       {isOpen && <CartModal closeModal={() => setIsOpen(false)} />}
-      <div onClick={() => setIsOpen(true)} className={styles.cart}>
+      <div
+        onClick={() => {
+          setIsOpen(true);
+          setIsAnimated(true);
+        }}
+        className={`${styles.cart} ${isAnimated && styles.animated}`}
+      >
         <CartIcon />
         <p>Your Cart</p>
         <span>{cartAmount || 0}</span>
